a344447e6690017297b55c1176917ab878704348
flotr18 <trouilletflorent@outlook.com>
2025-02-06 09:26:06 +0100

    Added Sator Square

--- palindrome.py
+++ palindrome.py
@@ -1,4 +1,8 @@
 def palindrome(chaine):
+    SatorSquare = ["sator","arepo","tenet","opera","rotas"]
+    if chaine in SatorSquare:
+        return True
     return chaine == chaine[::-1]
+    
 
 print(palindrome("radar"))
